e030e6383f67eff646b0d0791e55ab9ccc4d30f1
shrutilangeh <langehshruti108@gmail.com>
2026-03-07 16:01:03 +0530

    Add files via upload

---
+++ arrlist.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+public class arrlist {
+    public static void main(String []args){
+        ArrayList<String>arl =new ArrayList<>();
+        arl.add("Ram");
+        arl.add("Sham");
+        arl.add("Gansham");
+        arl.add("radhesham");
+        arl.add("Parshuram");
+        for(int i =0;i<arl.size();i++){
+ System.out.println(arl.get(i));
+        }
+       
+
+    }
+
+    
+}
