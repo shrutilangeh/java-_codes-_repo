202fea20ee1fbf13ee89004797c72e2b1800e2e4
shrutilangeh <langehshruti108@gmail.com>
2026-03-07 16:02:54 +0530

    Add files via upload

---
+++ arraytolist.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class arraytolist {
+    public static void main(String []args){
+    ArrayList<Integer> var1 = new ArrayList();
+      var1.add(1);
+      var1.add(2);
+      var1.add(3);
+      var1.add(4);
+      var1.add(5); 
+     
+    }
+    
+}
