2eebb1dd93e654e6967f0bc9aa4423f283da06af
shrutilangeh <langehshruti108@gmail.com>
2026-05-15 15:22:09 +0530

    Add files via upload

---
+++ P6.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		List<String> names = Arrays.asList("a", "b", "c");
+		Collections.reverse(names);
+		System.out.println(names);
+	}
+}
