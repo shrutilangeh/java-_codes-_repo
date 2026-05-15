e4446392d3cd8495fe3f74a8f82195f798051e34
shrutilangeh <langehshruti108@gmail.com>
2026-05-15 15:31:15 +0530

    Add files via upload

--- P3.java
+++ P3.java
@@ -1,19 +1,24 @@
-import java.util.Scanner;
-
-public class numberCheck{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-
-        
-        System.out.println("enter num: ");
-        String num = sc.nextLine();
-
-        try{
-            int number = Integer.parseInt(num);
-            System.out.println("number: "+ number);
-        }catch(NumberFormatException e){
-            System.out.println(" enter an integer value");
-        }
-        
+class myThread extends Thread {
+  public void run() {
+    for (int i = 0; i < 10; i++){
+      System.out.println("before sleep " + i);
+      try{
+        Thread.sleep(1000);
+      } catch (InterruptedException e){
+          System.out.println(e);
+            }
+      System.out.println("after sleep " + i);
     }
+  }
 }
+
+public class threadExample {
+  public static void main(String[] args){
+    myThread T1 = new myThread();
+    T1.start();
+    
+
+    System.out.println("main thread running.....");
+  }
+}
+    
