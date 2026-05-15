48f94adf2b6ef2ffa955f97ced20230ceb5abed6
shrutilangeh <langehshruti108@gmail.com>
2026-05-15 15:21:06 +0530

    Add files via upload

---
+++ P3.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    List<String> names = Arrays.asList("a", "b", "c");
+	    String target = "a";
+	    if(names.contains(target)){
+	            System.out.println(target + " is in list");
+	    }else{
+	        System.out.println(target + "is not in List");
+	    }
+	    
+		System.out.println("Hello World");
+	}
+}
