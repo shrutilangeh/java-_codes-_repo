7950b60e4304163aed12fa5ad199c886bfea39da
shrutilangeh <langehshruti108@gmail.com>
2026-05-15 15:32:43 +0530

    Add files via upload

--- P6.java
+++ P6.java
@@ -1,13 +1,43 @@
-public class finallyExample{
-    public static void main(String[] args){
-       try{
-            int number = 10/0
-            System.out.println("number: "+ number);
-        }catch(Exception e){
-            System.out.println(" error occured");
-        }finally{
-            System.out.println("finally block executed");
-        }
-        
+class Counter{
+    int count = 0;
+    
+    public synchronized void increment(){
+        count++;
     }
 }
+
+class myThread extends Thread{
+    Counter counter;
+    
+    public myThread(Counter counter){
+        this.counter = counter;
+    }
+    public void run(){
+        for (int i = 0; i<1000; i++){
+            counter.increment();
+        }
+    }
+}
+
+public class main{
+    public static void main(String[] args){
+        Counter counter = new Counter();
+        myThread t1 = new myThread(counter);
+        myThread t2 = new myThread(counter);
+        
+        t1.start();
+        t2.start();
+        
+        try{
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e){
+            System.out.println(e);
+            }
+
+        
+        System.out.println("final counter value : " + counter.count);
+    }
+}
+        
+        
