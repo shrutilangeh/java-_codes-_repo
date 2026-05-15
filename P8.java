7464c7ed8976d3dcf2fda8ab76bf4e11533aa57c
shrutilangeh <langehshruti108@gmail.com>
2026-05-15 15:28:17 +0530

    Add files via upload

--- P8.java
+++ P8.java
@@ -1,20 +1,14 @@
-import java.util.*;
-public class Main
-{
-    public static Map<String, Integer> countFrequencies(List<String> fruits){
-        Map<String, Integer> frequency = new HashMap<>();
-        
-        for (String fruit: fruits){
-            if (frequency.containsKey(fruit)){
-                frequency.put(fruit, frequency.get(fruit) + 1);
-            }else {
-                frequency.put(fruit, 1);
-            }
+public clas backAccount{
+    
+    static void withdraw(int amount, int balance){
+        if (amount > balance){
+            throw new ArithmeticException("insufficient balance");
+        } else{
+            balance = balance-amount;
+            System.out.println("amount withdraw success");
         }
-        return frequency;
     }
-	public static void main(String[] args) {
-		List<String> fruits = Arrays.asList("apple", "banana", "apple", "orange");
-		System.out.println(countFrequencies(fruits));
-	}
+    public static void main(String[] args){
+        withdraw(1000, 100);
+    }
 }
