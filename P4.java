bfd913e91ed4cfd15500f124c4353741c5ecffcd
shrutilangeh <langehshruti108@gmail.com>
2026-05-15 15:26:14 +0530

    Add files via upload

--- P4.java
+++ P4.java
@@ -1,13 +1,32 @@
-import java.util.*;
-public class Main
-{
-	public static void main(String[] args) {
-	    Integer[] numbers = {1,2,3,4};
-	    List<Integer> numberList = Arrays.asList(numbers);
-	    System.out.println("List: " + numberList);
-		
-		List<Integer> numList = Arrays.asList(4,5,6,7);
-		Integer[] numArray = numList.toArray(new Integer[0]);
-		System.out.println("Array: " + Arrays.toString(numArray));
-	}
+import java.util.Scanner;
+
+public class multipleCatchBlock{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        try{
+            System.out.println("enter num: ");
+            String num = sc.nextLine();
+            System.out.println("enter deno: ");
+            String deno = sc.nextLine();
+
+            int num1 = Integer.parseInt(num);
+            int deno1 = Integer.parseInt(deno);
+
+            int result = num1 / deno1;
+            System.out.println("result" + result);
+        }
+        catch(NumberFormatException){
+            System.out.println("enetr valid integer");
+            
+        }
+        catch(ArithmeticException e){
+            System.out.println(" annot divide by zero");
+        }
+        
+        catch(Exception e){
+            System.out.println(" invalid input");
+        }
+
+    }
 }
