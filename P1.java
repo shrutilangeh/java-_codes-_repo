11754da7f9d554177a79ef1dacbdb0175a770b02
shrutilangeh <langehshruti108@gmail.com>
2026-05-15 15:13:02 +0530

    Add files via upload

---
+++ P1.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Main
+{
+    public static void main(String[] args){
+	    ArrayList<String> names = new ArrayList<>();
+	    names.add("prnav");
+	    names.add("kesar");
+	    names.add("saksham");
+	    names.add("xvdw");
+	
+	    for (String name: names){
+	        System.out.println(name);
+	    }
+    }
+}
