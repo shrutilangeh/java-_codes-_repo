902711e2e7c11492f34893a7dd3c660c855fd804
shrutilangeh <langehshruti108@gmail.com>
2026-05-15 15:25:04 +0530

    Add files via upload

--- P3.java
+++ P3.java
@@ -1,15 +1,19 @@
-import java.util.*;
-public class Main
-{
-	public static void main(String[] args) {
-	    List<String> names = Arrays.asList("a", "b", "c");
-	    String target = "a";
-	    if(names.contains(target)){
-	            System.out.println(target + " is in list");
-	    }else{
-	        System.out.println(target + "is not in List");
-	    }
-	    
-		System.out.println("Hello World");
-	}
+import java.util.Scanner;
+
+public class numberCheck{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        
+        System.out.println("enter num: ");
+        String num = sc.nextLine();
+
+        try{
+            int number = Integer.parseInt(num);
+            System.out.println("number: "+ number);
+        }catch(NumberFormatException e){
+            System.out.println(" enter an integer value");
+        }
+        
+    }
 }
