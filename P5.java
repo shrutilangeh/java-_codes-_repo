40fe2ce93cab85fd095f2354af0a6cd9efe4f8e8
shrutilangeh <langehshruti108@gmail.com>
2026-05-15 15:32:24 +0530

    Add files via upload

--- P5.java
+++ P5.java
@@ -1,14 +1,35 @@
-import java.io.FileReader;
-import java.io.IOException;
-
-public class Filereaderexample{
-    public static void main(String[] args){
-        try(FileReader file = new FileReader("data.txt")){
-            System.out.println("file open succesfully");
-        }
-        catch(IOException e){
-            System.out.println("file not found..");
-            
-        }
+class myThread extends Thread {
+  public myThread(String name){
+      super(name);
+  }
+  public void run() {
+    for (int i = 0; i < 5; i++){
+      System.out.println("thread: " + Thread.currentThread().getName() +
+       " | priority: " + Thread.currentThread().getPriority() +
+       " value: " + i);
+      try{
+        Thread.sleep(500);
+      } catch (InterruptedException e){
+          System.out.println(e);
+            }
     }
+  }
 }
+
+public class threadExample {
+  public static void main(String[] args){
+    myThread T1 = new myThread("min priority thread");
+     myThread T2 = new myThread("normal priority thread");
+     myThread T3 = new myThread("max priority thread");
+    
+    T1.setPriority(Thread.MIN_PRIORITY);
+    T2.setPriority(Thread.NORM_PRIORITY);
+    T3.setPriority(Thread.MAX_PRIORITY);
+    
+    T1.start();
+    T2.start();
+    T3.start();
+
+  }
+}
+    
